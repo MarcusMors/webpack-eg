73b1a504d774fda0d8fce12374b4c7adcac4839a
Marcus Mors <alivezeh@gmail.com>
2021-04-09 21:00:02 -0500

    font loader, packages for optimizations are installed

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,18 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CopyPlugin = require("copy-webpack-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
 	entry: "./src/index.js",
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "dist"), //resolve return the absolute path in the OS
 		filename: "main.js",
+		assetModuleFilename: "assets/images/[hash][ext][query]",
 	},
 	resolve: {
-		extensions: ["js"],
+		extensions: ["js"], //the files that will be read
 	},
 	module: {
 		rules: [
@@ -39,11 +42,27 @@ module.exports = {
 				test: /\.png$/,
 				type: "asset/resource",
 			},
+			{
+				test: /\.(woff|woff2)$/,
+				use: {
+					loader: "url-loader",
+					options: {
+						limit: "10000", // or bool
+						mimetype: "application/font-woff", //specify the MIME type which the file will be aligned.
+						//MIME = Multipurpose Internet Mail Extensions
+						//it is the internet standard way to send info
+						name: "[name].[ext]",
+						outputPath: "./assets/fonts/",
+						publicPath: "./assets/fonts/",
+						esModule: false,
+					},
+				},
+			},
 		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			inject: true,
+			inject: true, //inject the bundle to the html template
 			template: "./public/index.html",
 			filename: "./index.html",
 		}),
